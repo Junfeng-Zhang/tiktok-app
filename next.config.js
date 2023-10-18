b76cf70db3f2400221ecc0a1ac6f8c45a3aa9ba3
Junfeng-Zhang <60849891+Junfeng-Zhang@users.noreply.github.com>
2023-10-18 13:16:06 +0800

    take back config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     webpack: (config, { isServer }) => {
-        // 添加一个规则来处理canvas.node二进制模块
+        // Add a rule to handle the canvas.node binary module
         config.module.rules.push({ test: /\.node$/, use: 'raw-loader' });
     
-        // 排除浏览器中的 Next.js 处理画布
+        // Exclude canvas from being processed by Next.js in the browser
         if (!isServer) config.externals.push('canvas');
         return config;
     },
